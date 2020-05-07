f0a11e1acc9b00d20d71da91f6a0b2f4c1d717b8
Rodolfo Barreira <rodolfo-barreira@hotmail.com>
2020-05-07 19:40:41 +0100

    controlador de admin

--- webapp/controller/HomeController.php
+++ webapp/controller/HomeController.php
@@ -5,12 +5,6 @@ use ArmoredCore\WebObjects\Redirect;
 use ArmoredCore\WebObjects\Session;
 use ArmoredCore\WebObjects\View;
 
-/**
- * Created by PhpStorm.
- * User: smendes
- * Date: 09-05-2016
- * Time: 11:30
- */
 class HomeController extends BaseController
 {
 
