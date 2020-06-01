60d00070c694312dea109aae9fcc6e5e832cce80
tiagomiguel218 <tiagosantos27062000@gmail.com>
2020-06-01 18:07:35 +0100

    backoffice update

--- webapp/model/Users.php
+++ webapp/model/Users.php
@@ -4,9 +4,11 @@ use ActiveRecord\Model;
 class Users extends \ActiveRecord\Model {
 
     static $validates_presence_of = array(
-        array('username', 'message' => 'YooaaH it must be provided'),
-        array('email', 'message' => 'YooaaH it must be provided'),
-        array('password', 'message' => 'YooaaH it must be provided')
+        array('username', 'message' => 'campo username deve ser preenchido'),
+        array('email', 'message' => 'campo email deve ser preenchido'),
+        array('password', 'message' => 'campo password deve ser preenchido'),
+        array('status', 'message' => 'campo status deve ser preenchido'),
+        array('role', 'message' => 'campo role deve ser preenchido'),
 
     );
 
